afa18f5fcfeaa09f8c9c9f1a4aeb6047fdc26480
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-04-15 11:19:17 +0200

    Refactor API design
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,42 +3,59 @@ extern crate gotham;
 extern crate gotham_middleware_lapin;
 extern crate hyper;
 extern crate lapin_futures as lapin;
-#[macro_use]
-extern crate log;
 extern crate mime;
 extern crate tokio_core;
 
-use futures::future::ok;
-use futures::Stream;
-use futures::future::Future;
+use futures::{future, Future};
 use hyper::StatusCode;
 use gotham::http::response::create_response;
 use gotham::handler::HandlerFuture;
 use gotham::state::State;
 use gotham::router::Router;
 use gotham::router::builder::*;
+use gotham::state::FromState;
 use gotham::pipeline::new_pipeline;
 use gotham::pipeline::single::single_pipeline;
 use gotham_middleware_lapin::{LapinChannel, LapinMiddleware};
-use lapin::client::ConnectionOptions;
-use lapin::channel::{BasicProperties, BasicPublishOptions, Channel, ConfirmSelectOptions,
-                     ExchangeDeclareOptions, QueueBindOptions, QueueDeclareOptions};
-use lapin::types::FieldTable;
-use tokio_core::reactor::Core;
-use tokio_core::net::TcpStream;
+use lapin::channel::{BasicProperties, BasicPublishOptions};
+use tokio_core::reactor::Handle;
 
-pub fn say_hello(state: State) -> Box<HandlerFuture> {
-    let res = create_response(
-        &state,
-        StatusCode::Ok,
-        Some((String::from("Hello, Lapin!").into_bytes(), mime::TEXT_PLAIN)),
-    );
+pub fn say_hello(mut state: State) -> Box<HandlerFuture> {
+    let sending = {
+        let handle = Handle::take_from(&mut state);
+        LapinChannel::borrow_from(&state).channel(&handle, |channel| {
+            let f = channel
+                .basic_publish(
+                    "raw_ex",
+                    "raw_2",
+                    format!("hello").as_bytes(),
+                    &BasicPublishOptions::default(),
+                    BasicProperties::default(),
+                )
+                .and_then(|_| future::ok(channel));
 
-    Box::new(ok((state, res)))
+            Box::new(f)
+        })
+    };
+
+    let f = sending.then(|res| {
+        if let Err(err) = res {
+            println!("error sending message: {}", err);
+        }
+
+        let res = create_response(
+            &state,
+            StatusCode::Ok,
+            Some((String::from("Hello, Lapin!").into_bytes(), mime::TEXT_PLAIN)),
+        );
+        Ok((state, res))
+    });
+
+    Box::new(f)
 }
 
-fn router(channel: Channel<TcpStream>) -> Router {
-    let lapin_mw = LapinMiddleware::new(channel);
+fn router() -> Router {
+    let lapin_mw = LapinMiddleware::new("127.0.0.1:5672".parse().unwrap());
 
     let (chain, pipelines) = single_pipeline(new_pipeline().add(lapin_mw).build());
 
@@ -48,84 +65,7 @@ fn router(channel: Channel<TcpStream>) -> Router {
 }
 
 pub fn main() {
-    // create the reactor
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let addr = "127.0.0.1:5672".parse().unwrap();
-    core.run(
-        TcpStream::connect(&addr, &handle)
-            .and_then(|stream| {
-                // connect() returns a future of an AMQP Client
-                // that resolves once the handshake is done
-                lapin::client::Client::connect(
-                    stream,
-                    &ConnectionOptions {
-                        frame_max: 65535,
-                        ..Default::default()
-                    },
-                )
-            })
-            .and_then(|(client, heartbeat_future_fn)| {
-                let heartbeat_client = client.clone();
-                handle.spawn(heartbeat_future_fn(&heartbeat_client).map_err(|_| ()));
-
-                client.create_confirm_channel(ConfirmSelectOptions::default())
-            })
-            .and_then(|channel| {
-                let id = channel.id;
-                println!("created channel with id: {}", id);
-
-                channel
-                    .queue_declare("raw", &QueueDeclareOptions::default(), &FieldTable::new())
-                    .map(|_| channel)
-            })
-            .and_then(|channel| {
-                println!("channel {} declared queue {}", channel.id, "raw");
-                channel
-                    .exchange_declare(
-                        "raw_ex",
-                        "direct",
-                        &ExchangeDeclareOptions::default(),
-                        &FieldTable::new(),
-                    )
-                    .map(|_| channel)
-            })
-            .and_then(|channel| {
-                println!("channel {} declared exchange {}", channel.id, "raw_ex");
-                channel
-                    .queue_bind(
-                        "raw",
-                        "raw_ex",
-                        "raw_2",
-                        &QueueBindOptions::default(),
-                        &FieldTable::new(),
-                    )
-                    .map(|_| channel)
-            })
-            .and_then(|channel| {
-                info!("channel {} declared queue {}", channel.id, "hello");
-
-                let addr = "127.0.0.1:7878";
-                println!("Listening for requests at http://{}", addr);
-                gotham::start(addr, router(channel.clone()));
-
-                channel
-                    .basic_publish(
-                        "raw_ex",
-                        "raw_2",
-                        b"hello from Gotham",
-                        &BasicPublishOptions::default(),
-                        BasicProperties::default(),
-                    )
-                    .join(ok(channel))
-            })
-            .map(|(confirmation, channel)| {
-                println!("publish got confirmation: {:?}", confirmation);
-                channel
-            })
-            .and_then(|channel| {
-                println!("closing amqp connection …");
-                channel.close(200, "Bye")
-            }),
-    ).expect("fail");
+    let addr = "127.0.0.1:7878";
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, router())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate amq_protocol;
 extern crate futures;
 extern crate gotham;
 #[macro_use]
@@ -8,32 +9,114 @@ extern crate log;
 extern crate tokio_core;
 
 use std::io;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
 
+use amq_protocol::types::FieldTable;
+use futures::{future, Future};
 use gotham::middleware::{Middleware, NewMiddleware};
 use gotham::state::{request_id, State};
 use gotham::handler::HandlerFuture;
-use lapin::channel::Channel;
-use tokio_core::net::TcpStream;
+use lapin::client::ConnectionOptions;
+use lapin::channel::{Channel, ConfirmSelectOptions, ExchangeDeclareOptions, QueueBindOptions,
+                     QueueDeclareOptions};
+use tokio_core::{net::TcpStream, reactor::Handle};
 
 #[derive(StateData)]
 pub struct LapinChannel {
-    channel: Channel<TcpStream>,
+    addr: SocketAddr,
+    channel: Arc<Mutex<Option<Channel<TcpStream>>>>,
 }
 
 impl LapinChannel {
-    pub fn channel(&self) -> Channel<TcpStream> {
-        self.channel.clone()
+    pub fn channel<CB>(&self, handle: &Handle, cb: CB) -> Box<Future<Item = (), Error = io::Error>>
+    where
+        CB: FnOnce(Channel<TcpStream>) -> Box<Future<Item = Channel<TcpStream>, Error = io::Error>>
+            + 'static,
+    {
+        let handle = handle.clone();
+        let channel = self.channel.lock().unwrap();
+        if let Some(ref channel) = *channel {
+            println!("channel {} available", channel.id);
+            Box::new(future::ok(()))
+        } else {
+            println!("no channel available, connecting to {}", self.addr);
+
+            let f = TcpStream::connect(&self.addr, &handle)
+                .and_then(|stream| {
+                    lapin::client::Client::connect(
+                        stream,
+                        &ConnectionOptions {
+                            frame_max: 65535,
+                            ..Default::default()
+                        },
+                    )
+                })
+                .and_then(move |(client, heartbeat_future_fn)| {
+                    let heartbeat_client = client.clone();
+                    handle.spawn(heartbeat_future_fn(&heartbeat_client).map_err(|_| ()));
+
+                    client.create_confirm_channel(ConfirmSelectOptions::default())
+                })
+                .and_then(|channel| {
+                    let id = channel.id;
+                    println!("created channel with id: {}", id);
+
+                    channel
+                        .queue_declare("raw", &QueueDeclareOptions::default(), &FieldTable::new())
+                        .map(|_| channel)
+                })
+                .and_then(|channel| {
+                    println!("channel {} declared queue {}", channel.id, "raw");
+                    channel
+                        .exchange_declare(
+                            "raw_ex",
+                            "direct",
+                            &ExchangeDeclareOptions::default(),
+                            &FieldTable::new(),
+                        )
+                        .map(|_| channel)
+                })
+                .and_then(|channel| {
+                    println!("channel {} declared exchange {}", channel.id, "raw_ex");
+                    channel
+                        .queue_bind(
+                            "raw",
+                            "raw_ex",
+                            "raw_2",
+                            &QueueBindOptions::default(),
+                            &FieldTable::new(),
+                        )
+                        .map(|_| channel)
+                })
+                .and_then(|channel| {
+                    println!("channel {} bound queue {}", channel.id, "raw");
+
+                    cb(channel)
+                })
+                .and_then(|channel| {
+                    println!("closing amqp connection …");
+                    channel.close(200, "Bye")
+                })
+                .and_then(|_| Ok(()));
+
+            Box::new(f)
+        }
     }
 }
 
 /// A Gotham compatible Middleware that lets you dispatch messages to an AMQP queue.
 pub struct LapinMiddleware {
-    channel: Channel<TcpStream>,
+    addr: SocketAddr,
+    channel: Arc<Mutex<Option<Channel<TcpStream>>>>,
 }
 
 impl LapinMiddleware {
-    pub fn new(channel: Channel<TcpStream>) -> Self {
-        LapinMiddleware { channel: channel }
+    pub fn new(addr: SocketAddr) -> Self {
+        LapinMiddleware {
+            addr: addr,
+            channel: Arc::new(Mutex::new(None)),
+        }
     }
 }
 
@@ -41,7 +124,10 @@ impl NewMiddleware for LapinMiddleware {
     type Instance = LapinMiddleware;
 
     fn new_middleware(&self) -> io::Result<Self::Instance> {
-        Ok(LapinMiddleware::new(self.channel.clone()))
+        Ok(LapinMiddleware {
+            addr: self.addr.clone(),
+            channel: self.channel.clone(),
+        })
     }
 }
 
@@ -52,6 +138,7 @@ impl Middleware for LapinMiddleware {
     {
         debug!("[{}] pre chain", request_id(&state));
         state.put(LapinChannel {
+            addr: self.addr.clone(),
             channel: self.channel.clone(),
         });
 
